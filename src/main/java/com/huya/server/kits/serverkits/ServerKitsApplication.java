d2d4258a967a0a12b957ad23677032e5d7544cd2
Kevinzhuo <wangzhuo648217@gamil.com>
2017-08-22 14:35:27 +0800

    Initial project

---
+++ src/main/java/com/huya/server/kits/serverkits/ServerKitsApplication.java
@@ -0,0 +1,12 @@
+package com.huya.server.kits.serverkits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerKitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerKitsApplication.class, args);
+	}
+}
